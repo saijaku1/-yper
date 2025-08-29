41315c06add362db16317a239de64d47e0a512b9
saijaku1 <souta20101024@outlook.jp>
2025-08-29 20:09:43 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,3 @@
 const button = document.querySelector('button').addEventListener('click', () => {
-  window.location.href = 'https://zetyper.f5.si/docs/intro';
+  window.location.href = 'https://zetyper.f5.si/docs/terms-of-service';
 }); 
