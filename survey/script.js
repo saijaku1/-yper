d825cda01e9a17d29f34c764ff28278706c91b0e
zetyper <jaku.typing@gmail.com>
2025-11-23 20:34:01 +0900

    Update script.js

--- survey/script.js
+++ survey/script.js
@@ -84,7 +84,7 @@ form.addEventListener("submit", function(event) {
   if (rank!==ranks.length-1){
     $("tonext").textContent = (rank+1)*150-score;
   };
-  $("rank").style.color = colors[rank];
+  $("rank").style.color = colors[rank]+"-webkit-text-stroke: 2px white";
 });
 $("reset").addEventListener("click", () => {
   inputs.forEach(id => {
